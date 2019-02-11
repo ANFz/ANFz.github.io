4ceb8720635666f0b3a7a0e9b0af79d56c5fda9d
ANFz <alexdapyro@gmail.com>
2019-02-11 12:32:25 -0700

    Create HomeMapJS.js

---
+++ HomeMapJS.js
@@ -0,0 +1,7 @@
+      var map;
+      function initMap() {
+        map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: 28.6934, lng: -81.510618},
+          zoom: 9
+        });
+      }
